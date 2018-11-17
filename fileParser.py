736198d6d579dfab1f7d3e56a7f0164849c3444d
geordie-quiroa <gqmail@protonmail.com>
2018-11-16 19:22:41 -0600

    nombres de variables mas descriptivas

--- fileParser.py
+++ fileParser.py
@@ -1,28 +1,27 @@
 from pygments import highlight
 from pygments.lexers.python import PythonLexer
 from pygments.formatters.html import HtmlFormatter
+import json
+files = []
 
 #from pygments import lex
-import json
 
-def read():
+
+def readFile():
     tmpFile = open('testFile.py', 'r').read()
     return tmpFile
 
-code = read()
-#print(code)
-array = list(PythonLexer().get_tokens(code))
+code = readFile()
+tokenObjects = tuple(PythonLexer().get_tokens(code))
 def convertToJson(token, text):
-    return {"token": str(token)[6:], "text": text}
-#print(array[2][0]) # lee las tuplas
-File2Json =  json.dumps({"tokens": [convertToJson(*token) for token in array]}, indent=2) # creo un multi-string con el formato de un json
-tmpFileJson = json.loads(File2Json) #creo un diccionario en base al string json anterior
-tmpTokens = tmpFileJson["tokens"]
-for obj in tmpTokens:
+    return {"token": str(token)[6:], "value": text}
+File2Json =  json.dumps([convertToJson(*token) for token in tokenObjects], indent=2) # creo un multi-string con el formato de un json
+tmpFileJsonArray = json.loads(File2Json) #creo un diccionario en base al string json anterior
+for obj in tmpFileJsonArray:
     if obj["token"] == "Name.Class":
-        print(obj["text"])
+        print(obj["value"])
     #print(token)
-    
+print(tmpFileJsonArray)   
 #print(array)
 #print(tmpFileJson["tokens"])
 
